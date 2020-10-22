8f6b44e755e617fe14f94c8bdaac2dd4071450ba
Alle Nikhil <45490591+allenikhil@users.noreply.github.com>
2020-10-22 11:34:47 +0530

    Add files via upload
    kk

--- JavaCollectionsExamples/src/com/java/HashTable/HashTableExample.java
+++ JavaCollectionsExamples/src/com/java/HashTable/HashTableExample.java
@@ -14,8 +14,17 @@ public class HashTableExample {
 		marks.put("tancredi", new Integer(800)); 
 		marks.put("bellick", new Integer(435)); 
 		marks.put("null", new Integer(435)); 
-		marks.put("null", new Integer(435)); 
+		marks.put("null", new Integer(436)); 
 
+		// provide value for new key which is absent 
+				// using computeIfAbsent method 
+		marks.computeIfAbsent("Pen", k -> 600); 
+		marks.computeIfAbsent("Book", k -> 800); 
+
+				// print new mapping 
+				System.out.println("new hashTable: "
+								+ marks); 
+				
 		// check whether a value exists or not 
 		if (marks.containsKey("null")) 
 			System.out.println("Key found in table"); 
@@ -36,5 +45,36 @@ public class HashTableExample {
   
         // printing set entries 
         System.out.println("set entries: " + s); 
+        
+        
+        Hashtable<String, Integer> marks1 = (Hashtable<String, Integer>) marks.clone(); 
+
+		 // checking whether both hash tables  
+        // are equal or not 
+        if (marks.equals(marks1)) 
+            System.out.println("both hash tables are equal");
+        else
+        	System.out.println("both hash tables are Not equal");
+        
+        // printing Hastable entries 
+        //System.out.println("HashTable entries: " + marks); 
+        //System.out.println("HashTable entries: " + marks1); 
+        
+        HashMap<String,Integer> hm=new HashMap<String,Integer>(); 
+        hm.put("tweener", new Integer(345)); 
+		hm.put("krantz", new Integer(100)); 
+		hm.put("burrows", new Integer(790)); 
+		hm.put("tancredi", new Integer(800)); 
+		hm.put("bellick", new Integer(435)); 
+		hm.put("null", new Integer(435)); 
+		hm.put("null", new Integer(436)); 
+        
+        // checking whether  both hash table and Hashmap   
+        // are equal or not 
+        if (marks1.equals(hm)) 
+            System.out.println("both hash table and Hashmap are equal");
+        else
+        	System.out.println("both are hash table and Hashmap  Not equal");
+        
 	} 
 }
